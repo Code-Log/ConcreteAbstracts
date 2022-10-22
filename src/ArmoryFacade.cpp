2d471b4e2d925c50bc8bcae40df9823d73a6df51
andreas <u21430901@tuks.co.za>
2022-10-22 11:42:25 +0200

    add ArmoryFacade

--- src/ArmoryFacade.cpp
+++ src/ArmoryFacade.cpp
@@ -1,20 +1,37 @@
 #include <ArmoryFacade.h>
 
 ArmoryFacade::ArmoryFacade() {
+    weaponTransport = new WeaponTransport();
+    suppliesTransport = new SuppliesTransport();
+    attackVessel = new AttackVessel();
 
+    transportContext = new TransportContext(weaponTransport);
 }
 
 bool ArmoryFacade::purchaseWeapon(Recruits *recruits)
 {
-
+    transportContext->setStrategy(weaponTransport);
+    // TODO: Implement ArmoryFacade::purchaseWeapon
+    return true;
 }
 
 bool ArmoryFacade::purchaseAttackVessel(Recruits *recruits)
 {
-
+    transportContext->setStrategy(attackVessel);
+    // TODO: Implement ArmoryFacade::purchaseAttackVessel
+    return true;
 }
 
 bool ArmoryFacade::purchaseSupplies(Recruits *recruits)
 {
+    transportContext->setStrategy(suppliesTransport);
+    // TODO: Implement ArmoryFacade::purchaseSupplies
+    return true;
+}
 
+ArmoryFacade::~ArmoryFacade() {
+    delete transportContext;
+    delete weaponTransport;
+    delete suppliesTransport;
+    delete attackVessel;
 }
