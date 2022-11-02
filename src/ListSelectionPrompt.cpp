1577ce99bca3120439a5f1782de7289803d16f8f
Jaco Malan <jacomalan.12@gmail.com>
2022-11-02 13:29:15 +0200

    Added ListSelectionPrompt struct

---
+++ src/ListSelectionPrompt.cpp
@@ -0,0 +1,25 @@
+#include <util/ListSelectionPrompt.h>
+#include <iostream>
+
+ListSelectionPrompt::ListSelectionPrompt(std::initializer_list<std::string> init)
+    : options(init)
+{
+}
+
+std::string ListSelectionPrompt::getSelection(const std::string& prompt) const
+{
+    int maxVal = options.size();
+    for (int i = 1; i < options.size() + 1; i++)
+        std::printf("%d. %s\n", i, options[i - 1].c_str());
+
+
+    int selection;
+    do {
+        std::cout << prompt;
+        std::cin >> selection;
+        if (selection <= 0 || selection > maxVal)
+            std::cout << "Invalid selection!" << std::endl;
+    } while (selection <= 0 || selection > maxVal);
+
+    return std::move(options[selection - 1]);
+}
