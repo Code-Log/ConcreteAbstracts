ac8d8bf23df78251ca9eb1416abdb01d5fd284cd
TumiPare <u21452832@tuks.co.za>
2022-11-02 14:39:22 +0200

    Added empty functions for WarEngine

--- src/WarEngine.cpp
+++ src/WarEngine.cpp
@@ -50,8 +50,126 @@ WarEngine& WarEngine::getInstanceWarEngine()
     static WarEngine onlyInstance_;
     return onlyInstance_;
 }
-
+/**
+ * @brief Destroy the War Engine:: War Engine object
+ * 
+ */
 WarEngine::~WarEngine()
+{
+    
+}
+
+void WarEngine::run()
+{
+    
+}
+
+void WarEngine::stopEngine()
+{
+    
+}
+
+void WarEngine::runEngine()
+{
+    
+}
+
+void WarEngine::runGUIEngine()
+{
+    
+}
+
+void WarEngine::prePhase1()
+{
+    
+}
+
+void WarEngine::phase1()
+{
+    
+}
+
+void WarEngine::phase2()
+{
+    
+}
+
+void WarEngine::phase3()
+{
+    
+}
+
+void WarEngine::selectCountry()
+{
+    
+}
+
+void WarEngine::selectPoliticalRegime()
+{
+    
+}
+
+void WarEngine::setAllies()
+{
+    
+}
+
+void WarEngine::partitionRecruite()
+{
+    
+}
+
+void WarEngine::buyAndDestributeWeapons()
+{
+    
+}
+
+void WarEngine::setWarTheatres()
+{
+    
+}
+
+void WarEngine::setTraps()
+{
+    
+}
+
+void WarEngine::printEngineReport()
+{
+    
+}
+
+void WarEngine::makeDecision(Country* c)
+{
+    
+}
+
+void WarEngine::increaseAllies()
+{
+    
+}
+
+void WarEngine::sendRecruitAndAttack()
+{
+    
+}
+
+void WarEngine::sendRecruit()
+{
+    
+}
+
+void WarEngine::buyAndSetTraps()
+{
+    
+}
+
+void WarEngine::surrender()
+{
+    
+}
+
+void WarEngine::PrintWarReport()
 {
     
 }
