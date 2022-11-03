22a90d27c8967174dca52b60baf155fe69990870
AmberLz <u18004874@tuks.co.za>
2022-11-03 15:58:12 +0200

    Add randomNumGenerator method
    Call the method and generate a random number within range.

--- src/WarEngine.cpp
+++ src/WarEngine.cpp
@@ -320,3 +320,10 @@ WarEngine::WarEngine()
     
 }
 
+int WarEngine::randomNumGenerator(int min, int max)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dist(min, max);
+  return dist(gen);
+}
