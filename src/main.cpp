d93e14d0a07488d666a93b564446a8dfd7c40dc7
TumiPare <u21452832@tuks.co.za>
2022-11-06 12:53:43 +0200

    just my main

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,11 @@ int main() {
     //         std::cout<<c->getName()<<std::endl;
     // }
     // WarEngine::getInstanceWarEngine().selectPoliticalRegime(false);
-    WarEngine::getInstanceWarEngine().prePhase1(true);
-    WarEngine::getInstanceWarEngine().phase1(true);
-    WarEngine::getInstanceWarEngine().setAllies(true);
+
+    WarEngine::getInstanceWarEngine().setHuman(false);
+    WarEngine::getInstanceWarEngine().prePhase1();
+    WarEngine::getInstanceWarEngine().phase1();
+    WarEngine::getInstanceWarEngine().setAllies();
     // WarEngine::getInstanceWarEngine().partitionRecruits();
 
     std::cout<<colours::CYAN_UNDERLINED<<"hello world"<<colours::RESET<<std::endl;
